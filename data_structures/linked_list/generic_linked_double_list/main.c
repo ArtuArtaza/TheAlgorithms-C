5693aa4c206baee3213d10c22bce63736b243290
dengking <2238897886@qq.com>
2019-10-10 20:37:04 +0800

    add test case to adlist

--- data_structures/linked_list/generic_linked_double_list/main.c
+++ data_structures/linked_list/generic_linked_double_list/main.c
@@ -4,4 +4,29 @@
  *  Created on: 2019Äê10ÔÂ9ÈÕ
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "adlist.h"
+
+typedef struct S {
+	int id;
+} S;
+
+int main() {
+	list * my_list = listCreate();
+	S* s = NULL;
+	for (int id = 0; id < 10; ++id) {
+		s = malloc(size(*s));
+		if (s == NULL) {
+			printf("out of memory");
+			return -1;
+		}
+		s->id = id;
+		if(listAddNodeTail(my_list, s) == NULL){
+			printf("adding node to tail of the list fails");
+			return -1;
+		}
+	}
+	return 0;
+}
